6adcc804a5a69fb5e065ad2d672ab241aec606b2
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2025-07-11 06:34:40 +0000

    [init] first init

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 sudo apt update
 sudo apt install tmux sox ffmpeg -y
+sudo apt install libsox-dev
 tmux set-option -g mouse on \; bind -T copy-mode-vi MouseDragEnd1Pane send -X copy-selection-and-cancel
 
 pip install -e .
+pip install kaldiio
