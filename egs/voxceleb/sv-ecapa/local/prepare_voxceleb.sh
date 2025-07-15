3ca0cab470e1db1e18d0b7287d0bfdb6daee8e90
yangwang <yangwang4work@gmail.com>
2025-07-15 07:00:35 +0000

    [init] first init

--- egs/voxceleb/sv-ecapa/local/prepare_voxceleb.sh
+++ egs/voxceleb/sv-ecapa/local/prepare_voxceleb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-python prepare_voxceleb.py \
+python local/prepare_voxceleb.py \
     --voxceleb_root /home/jovyan/corpus/audio/voxceleb1 \
     --manifest_root /home/jovyan/workspace/vibe/manifests \
     --manifest_prefix voxceleb1 \
     --max_workers 32
 
-python prepare_voxceleb.py \
+python local/prepare_voxceleb.py \
     --voxceleb_root /home/jovyan/corpus/audio/voxceleb2 \
     --manifest_root /home/jovyan/workspace/vibe/manifests \
     --manifest_prefix voxceleb2 \
