9f9d99de802aa047f20a1f31276f3aeee794cde7
yangwang <yangwang4work@gmail.com>
2025-08-16 05:33:40 +0000

    [feat] include losses in init

---
+++ src/vibe/losses/__init__.py
@@ -0,0 +1,8 @@
+from .margin_loss import (
+    ArcFaceLoss, 
+    ArcMarginLoss, 
+    CosFaceLoss, 
+    AddMarginLoss, 
+    SphereFaceLoss
+)
+from .chebyshev_margin_loss import ChebyshevArcMarginLoss
